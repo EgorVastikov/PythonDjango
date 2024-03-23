06d071200f227264db648cc31a0d101d46b38730
Egor Vastikov <egorvast@mail.ru>
2024-03-23 15:39:38 +0500

    -

--- myproject/myapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Note
-from .models import Human
-
-admin.site.register(Note)
-admin.site.register(Human)
-# Register your models here.
--- myproject/myapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Note
-
-class ModelForm(forms.ModelForm):
-    class Meta:
-        model = Note
-        fields = ['title', 'text']
